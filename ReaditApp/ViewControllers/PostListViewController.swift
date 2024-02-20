3579b8e74a9263738228ab232205ff2204f30769
Mariana Piz <marianapiz0412@gmail.com>
2024-02-20 19:17:31 +0000

    Implement paging

--- ReaditApp/ViewControllers/PostListViewController.swift
+++ ReaditApp/ViewControllers/PostListViewController.swift
@@ -13,21 +13,25 @@ class PostListViewController: UIViewController {
     struct Const {
         static let cellReuseIdentifier = "post"
         static let goToPostViewSegueID = "go_to_post_view"
+        static let subredditTopic = "cats"
     }
     
     // MARK: - Properties & data
     var posts: [RedditPost] = []
     var selectedPost: RedditPost?
+    var afterId: String?
+    var isFetchingPosts = false
 
-    // MARK: - IBOutlet
+    // MARK: - IBOutlets
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var subredditLabel: UILabel!
     
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.tableView.rowHeight = 300;
-        setPostValues()
+        manageUI()
+        fetchPosts()
     }
     
     // MARK: - Navigation
@@ -44,20 +48,46 @@ class PostListViewController: UIViewController {
         }
     }
     
-    private func setPostValues() {
+    // MARK: - UI Management
+    private func manageUI() {
+        self.tableView.rowHeight = 300;
+        self.subredditLabel.text = "/r/\(Const.subredditTopic)"
+    }
+    
+    private func fetchPosts(loadMore: Bool = false) {
+        guard !isFetchingPosts else { return }
+        isFetchingPosts = true
+
         let apiService = APIService()
         Task {
             do {
-                let redditResponse = try await apiService.fetchPosts(subreddit: "capybara", limit: 10, after: nil)
+                let redditResponse = try await apiService.fetchPosts(subreddit: Const.subredditTopic, limit: 20, after: loadMore ? afterId : nil)
                 DispatchQueue.main.async { [weak self] in
-                    self?.posts = redditResponse.data.children.map { $0 }
-                    self?.tableView.reloadData()
+                    guard let self = self else { return }
+                    
+                    if loadMore && redditResponse.data.after == self.afterId {
+                        // If afterId is the same, there are no more posts to load
+                        self.isFetchingPosts = false
+                        return
+                    }
+                    
+                    if loadMore {
+                        self.posts += redditResponse.data.children
+                    } else {
+                        self.posts = redditResponse.data.children
+                    }
+                    
+                    self.afterId = redditResponse.data.after
+                    self.tableView.reloadData()
+                    self.isFetchingPosts = false
                 }
             } catch {
                 print("Failed with error: \(error)")
+                self.isFetchingPosts = false
             }
         }
     }
+    
 }
 
 // MARK: - UITableViewDataSource
@@ -84,6 +114,7 @@ extension PostListViewController: UITableViewDataSource {
 
 // MARK: - UITableViewDelegate
 extension PostListViewController: UITableViewDelegate {
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.selectedPost = self.posts[indexPath.row]
         self.performSegue(
@@ -91,4 +122,16 @@ extension PostListViewController: UITableViewDelegate {
             sender: indexPath
         )
     }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let contentHeight = scrollView.contentSize.height
+        let height = scrollView.frame.height
+
+        // a bit in advance
+        if offsetY > contentHeight - height * 3 {
+            fetchPosts(loadMore: true)
+        }
+    }
+
 }
